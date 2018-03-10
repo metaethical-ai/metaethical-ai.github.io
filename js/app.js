d3b507cfe72df9a318aead87c3eafafc1061a696
June Ku <june@metaethical.ai>
2018-03-09 23:57:04 -0800

    refactor

---
+++ js/app.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+	set_width();
+	$(window).resize(function() {
+		set_width();
+	});
+	var email = 'june' + '@' + 'metaethical.ai';
+	$('#mailto').attr('href', 'mailto://' + email);
+	$('#mailto').html(email);
+});
+
+set_width = function() {
+	if (window.innerHeight < window.innerWidth) {
+		var width =	parseInt($(window).width()) - 
+								parseInt($('#left').css('width')) - 5 + 'px';
+		$('#right').css('width', width);
+	}
+}
+
+toggle_page = function() {
+	var home_active = parseInt($('#home').css('top')) == 0;
+	speed = 600;
+	if (home_active) {
+		active = '#home';
+		other = '#abstract';
+		$(active).animate({ top: '100vh' }, speed, 'swing');
+		$(other).animate({ top: '0' }, speed, 'swing');
+	} else {
+		active = '#abstract';
+		other = '#home';
+		$(active).animate({ top: '-100vh' }, speed, 'swing');
+		$(other).animate({ top: '0' }, speed, 'swing');
+	}
+}
+
